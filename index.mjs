2250759b9559de787f7b279f18316636a2132289
Illia Rayetzki <rayetzkiillya@gmail.com>
2022-11-09 08:17:57 +0200

    [JS30]: Check how much is done

---
+++ index.mjs
@@ -0,0 +1,20 @@
+import { readdir, stat } from 'fs/promises';
+
+let count = 0;
+
+try {
+  const allFiles = await readdir('./');
+  
+  for await (const dir of allFiles) {
+    const stats = await stat(dir);
+    if (stats.isDirectory()) {
+      const files = await readdir(dir);
+      const ready = files.filter(file => file.includes('FINISHED') || file.includes('START'));
+      if (ready.length === 0) count++;
+    };
+  }
+
+  console.info(`Challenges: ${count} out of ${allFiles.length} finished`);
+} catch (err) {
+  console.error('Current directory could not be read');
+}
