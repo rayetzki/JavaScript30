6d95e24501905fd1786418481f36998e0f44c6c5
Illia Rayetzki <rayetzkiillya@gmail.com>
2022-11-15 08:06:08 +0200

    [JS30]: Video speed controller

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import { readdir, stat } from 'fs/promises';
 import { parse } from 'path'
 
 let all = 0;
-let finished = 0;
+let finished = -1;
 
 try {
   const allFiles = await readdir('./');
@@ -13,11 +13,11 @@ try {
       const files = await readdir(dir);
       const ready = files.filter(file => file.includes('FINISHED') || file.includes('START'));
       if (ready.length === 0) finished++;
-      if (files.some(file =>parse(file).ext.includes('html') && parse(file).name.includes('index'))) all++;
+      if (files.some(file => parse(file).ext.includes('html') && parse(file).name.includes('index'))) all++;
     };
   }
 
-  console.info(`Challenges: ${finished} out of ${all} finished`);
+  console.info(`Challenges: ${finished} out of ${all} finished. ${all - finished} to go!`);
 } catch (err) {
   console.error('Current directory could not be read');
 }
